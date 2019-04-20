92a1c3d73df59f49a610c9b5df1ecc821b1b0456
m-daichi1219 <matu9525@gmail.com>
2019-04-20 20:59:05 +0900

    next 7.3.4

---
+++ ch07/train_seq2seq.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset import sequence
+from common.optimizer import Adam
+from common.trainer import Trainer
+from common.util import eval_seq2seq
+from ch07.seq2seq import Seq2seq
+# PeekySeq2seq??
+# TODO:7.3.4
