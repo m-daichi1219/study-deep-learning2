a665f2d031fc7a976b17197db99835047e5522fc
m-daichi1219 <matu9525@gmail.com>
2019-04-22 01:18:32 +0900

    ch07 completion(but peeky_seq2seq don't run properly. need fix it.)

--- ch07/train_seq2seq.py
+++ ch07/train_seq2seq.py
@@ -5,5 +5,63 @@ from common.optimizer import Adam
 from common.trainer import Trainer
 from common.util import eval_seq2seq
 from ch07.seq2seq import Seq2seq
-# PeekySeq2seq??
-# TODO:7.3.4
+from ch07.peeky_seq2seq import PeekySeq2seq
+
+
+# データセットの読み込み
+(x_train, t_train), (x_test, t_test) = sequence.load_data('addition.txt')
+char_to_id, id_to_char = sequence.get_vocab()
+
+# 改良①:入力データの反転
+x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
+
+# ハイパーパラメータの設定
+vocab_size = len(char_to_id)
+wordvec_size = 16
+hidden_size = 128
+batch_size = 128
+max_epoch = 25
+max_grad = 5.0
+
+# モデル / オプティマイザ / トレーナーの生成
+# model = Seq2seq(vocab_size, wordvec_size, hidden_size)
+# 改良②:PeekySeq2seq(エンコードされた入力データのベクトルhをすべてのレイヤで使用する）
+model = PeekySeq2seq(vocab_size, wordvec_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+acc_list = []
+for epoch in range(max_epoch):
+    trainer.fit(x_train, t_train, max_epoch=1, batch_size=batch_size, max_grad=max_grad)
+
+    correct_num = 0
+    for i in range(len(x_test)):
+        question, correct = x_train[[i]], t_test[[i]]
+        verbose = i < 10
+        correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose)
+
+    acc = float(correct_num) / len(x_test)
+    acc_list.append(acc)
+    print('val acc %.3f%%' % (acc * 100))
+
+# 改良前：少しずつだが、着実に学習している
+# | epoch 1 |  iter 1 / 351 | time 0[s] | loss 2.56
+# | epoch 1 |  iter 21 / 351 | time 1[s] | loss 2.53
+# | epoch 1 |  iter 41 / 351 | time 2[s] | loss 2.17
+# ...
+# val acc 0.040%
+# | epoch 5 |  iter 1 / 351 | time 0[s] | loss 1.28
+# | epoch 5 |  iter 21 / 351 | time 0[s] | loss 1.29
+# | epoch 5 |  iter 41 / 351 | time 1[s] | loss 1.28
+
+# 改良①:学習の進みが早くなる
+#           変換前と変換後の単語の位置（時系列）が近くなるため、
+#           学習の進みが早くなると考えられている（理論的なことはわかっていない。。）
+# | epoch 1 |  iter 1 / 351 | time 0[s] | loss 2.56
+# | epoch 1 |  iter 21 / 351 | time 0[s] | loss 2.52
+# | epoch 1 |  iter 41 / 351 | time 1[s] | loss 2.17
+# ...
+# val acc 0.080%
+# | epoch 5 |  iter 1 / 351 | time 0[s] | loss 1.01
+# | epoch 5 |  iter 21 / 351 | time 0[s] | loss 1.01
+# | epoch 5 |  iter 41 / 351 | time 1[s] | loss 1.00
